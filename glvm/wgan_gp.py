11e54ff33e6b86e2b87213b8620676a4e19ad24f
zhangdongkun98 <zhangdongkun98@gmail.com>
2021-10-18 20:04:57 +0800

    sth

--- glvm/wgan_gp.py
+++ glvm/wgan_gp.py
@@ -9,28 +9,8 @@ from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 
 
-# class GradientPaneltyLoss(nn.Module):
-#     def __init__(self):
-#          super(GradientPaneltyLoss, self).__init__()
 
-#     def forward(self, y, x):
-#         """Compute gradient penalty: (L2_norm(dy/dx) - 1)**2."""
-#         weight = torch.ones_like(y)
-#         dydx = torch.autograd.grad(outputs=y,
-#                                    inputs=x,
-#                                    grad_outputs=weight,
-#                                    retain_graph=True,
-#                                    create_graph=True,
-#                                    only_inputs=True)[0]
-
-#         dydx = dydx.view(dydx.size(0), -1)
-#         dydx_l2norm = torch.sqrt(torch.sum(dydx ** 2, dim=1))
-#         return torch.mean((dydx_l2norm - 1) ** 2)
-
-
-
-
-class WGANGP(rllib.template.Method):
+class WganGp(rllib.template.Method):
     lr_g = 1e-4
     lr_d = 1e-4
     weight_decay = 5e-4
@@ -53,6 +33,7 @@ class WGANGP(rllib.template.Method):
         self.discriminator = config.get('net_discriminator', Discriminator)(config).to(self.device)
         self.generator = config.get('net_generator', Generator)(config).to(self.device)
         self.models_to_save = [self.discriminator, self.generator]
+        self.models_to_load = [self.discriminator, self.generator]
 
         self.d_optimizer = RMSprop(self.discriminator.parameters(), lr=self.lr_d, weight_decay=self.weight_decay)
         self.g_optimizer = RMSprop(self.generator.parameters(), lr=self.lr_g, weight_decay=self.weight_decay)
@@ -113,26 +94,15 @@ class WGANGP(rllib.template.Method):
 
 
     def calculate_gradient_penalty(self, real, fake):
-    
-
-        """Calculates the gradient penalty loss for WGAN GP"""
         alpha = torch.rand((self.batch_size, 1), device=self.device)
-        # Get random interpolation between real and fake data
         interpolates = (alpha * real.data + ((1 - alpha) * fake.data)).requires_grad_(True)
 
         model_interpolates = self.discriminator(interpolates)
-        # grad_outputs = torch.ones(model_interpolates.size(), device=self.device, requires_grad=False)
-        # grad_outputs = torch.ones_like(model_interpolates, requires_grad=False)
         grad_outputs = torch.ones_like(model_interpolates)
 
-        # Get gradient w.r.t. interpolates
         gradients = grad(
-            outputs=model_interpolates,
-            inputs=interpolates,
-            grad_outputs=grad_outputs,
-            create_graph=True,
-            retain_graph=True,
-            only_inputs=True,
+            outputs=model_interpolates, inputs=interpolates, grad_outputs=grad_outputs,
+            create_graph=True, retain_graph=True, only_inputs=True,
         )[0]
         gradients = gradients.view(gradients.size(0), -1)
         gradient_penalty = torch.mean((gradients.norm(2, dim=1) - 1) **2)
@@ -140,13 +110,34 @@ class WGANGP(rllib.template.Method):
 
 
 
+class Evaluate(rllib.template.Method):
+    dim_noise = 64
+
+    def __init__(self, config: rllib.basic.YamlConfig, writer: rllib.basic.Writer):
+        super().__init__(config, writer)
+
+        config.set('method_name', 'WganGp'.upper())
+        config.set('dim_noise', self.dim_noise)
+
+        self.discriminator = config.get('net_discriminator', Discriminator)(config).to(self.device)
+        self.generator = config.get('net_generator', Generator)(config).to(self.device)
+        # self.models_to_load = [self.discriminator, self.generator]
+        self.models_to_load = [self.generator]
+        self._load_model()
+        return
+
+
 
 class Generator(rllib.template.Model):
+    """
+        config: dim_data
+    """
+
     def __init__(self, config):
         super(Generator, self).__init__(config, model_id=0)
 
-        ###! warning   todo
-        dim_input, dim_output = 1, 1
+        dim_input = config.dim_noise
+        dim_output = config.dim_data
 
         self.fe = FeatureMapper(config, 0, dim_input, dim_output)
         self.apply(rllib.utils.init_weights)
@@ -156,13 +147,16 @@ class Generator(rllib.template.Model):
 
 
 class Discriminator(rllib.template.Model):
+    """
+        config: dim_data
+    """
+
     def __init__(self, config):
         super(Discriminator, self).__init__(config, model_id=0)
 
-        ###! warning   todo
-        dim_input, dim_output = 1, 1
+        dim_input = config.dim_data
 
-        self.fe = FeatureMapper(config, 0, dim_input, dim_output)
+        self.fe = FeatureMapper(config, 0, dim_input, 1)
         self.apply(rllib.utils.init_weights)
 
     def forward(self, x):
